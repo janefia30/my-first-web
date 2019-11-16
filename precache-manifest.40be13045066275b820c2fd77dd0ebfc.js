5104a0451d81b4420b7df619fb80df984ac9240f
M-Rangga <janefia30>
2019-11-16 14:08:11 +0700

    first commit

---
+++ precache-manifest.40be13045066275b820c2fd77dd0ebfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a53f3dc0007072b9484e2ea4e0ed825",
+    "url": "/static/media/image2.3a53f3dc.png"
+  },
+  {
+    "revision": "7768ba49504d4da87a1da8eb2095cf62",
+    "url": "/static/media/chevron-down.7768ba49.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe4f380882affc1a933d",
+    "url": "/static/js/main.f4b7707a.chunk.js"
+  },
+  {
+    "revision": "a52fa2e3d750a5559fc2",
+    "url": "/static/js/2.c202c282.chunk.js"
+  },
+  {
+    "revision": "fe4f380882affc1a933d",
+    "url": "/static/css/main.46af51dc.chunk.css"
+  },
+  {
+    "revision": "cc4c63d801cb97d9aa045eef607155c7",
+    "url": "/index.html"
+  }
+];
